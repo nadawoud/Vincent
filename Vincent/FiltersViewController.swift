38b6f83b251f3daddb4d438460cb0d9296a5d4a1
Nada Yehia Dawoud <nadawoud@gmail.com>
2022-06-07 14:19:54 +0200

    Hook up IBOutlets and add a filter to the image in the button IBAction

--- Vincent/FiltersViewController.swift
+++ Vincent/FiltersViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import GPUImage
 
 class FiltersViewController: UIViewController {
 
+    @IBOutlet weak var renderView: RenderView!
+    @IBOutlet weak var filterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func filterButtonTapped(_ sender: UIButton) {
+        let picture = PictureInput(image: UIImage(named: "starry-night")!)
+        let filter = SaturationAdjustment()
+        picture --> filter --> renderView
+        picture.processImage()
+    }
 }
 
