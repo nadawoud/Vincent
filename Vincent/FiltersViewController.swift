6f9431249a59febdaeccf75c9efcde6ccc1b11eb
Nada Yehia Dawoud <nadawoud@gmail.com>
2022-06-08 08:39:36 +0200

    Create five image filters and display thumbnails of their effects

--- Vincent/FiltersViewController.swift
+++ Vincent/FiltersViewController.swift
@@ -6,23 +6,83 @@
 //
 
 import UIKit
-import GPUImage
+import CoreImage
+import CoreAudio
+import CoreMedia
 
 class FiltersViewController: UIViewController {
-
-    @IBOutlet weak var renderView: RenderView!
-    @IBOutlet weak var filterButton: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var filterButtons: [UIButton]!
+    
+    let context = CIContext()
+    let image = UIImage(named: "starry-night")
+    let filters = [CIFilter(name: "CISepiaTone"),
+                   CIFilter(name: "CIPhotoEffectNoir"),
+                   CIFilter(name: "CIColorMonochrome"),
+                   CIFilter(name: "CIColorInvert"),
+                   CIFilter(name: "CIVignetteEffect")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createThumbnails()
     }
-
-    @IBAction func filterButtonTapped(_ sender: UIButton) {
-        let picture = PictureInput(image: UIImage(named: "starry-night")!)
-        let filter = SaturationAdjustment()
-        picture --> filter --> renderView
-        picture.processImage()
+    
+    func createThumbnails() {
+        let thumbnailSize = CGSize(width: filterButtons[0].frame.size.width * 0.9, height: filterButtons[0].frame.size.height * 0.9)
+        for index in filterButtons.indices {
+            guard let cgImage = applyFilter(filters[index]) else { return }
+            let uiImage = UIImage(cgImage: cgImage)
+            let thumbnail = uiImage.preparingThumbnail(of: thumbnailSize)
+            filterButtons[index].setImage(thumbnail, for: .normal)
+        }
+    }
+    
+    func applyFilter(_ filter: CIFilter?, withIntensity intensity: NSNumber = 0.5, andColor color: CIColor = CIColor(color: .blue)) -> CGImage? {
+        guard let filter = filter else { return nil }
+        
+        guard let uiImage = image else { return nil }
+        let ciImage = CIImage(image: uiImage)
+        
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        if filter.inputKeys.contains("inputIntensity") {
+            filter.setValue(intensity, forKey: kCIInputIntensityKey)
+        }
+        
+        if filter.inputKeys.contains("inputColor") {
+            filter.setValue(color, forKey: kCIInputColorKey)
+        }
+        
+        guard let outputImage = filter.outputImage else { return nil }
+        
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+        return cgImage
+    }
+    
+    @IBAction func sepiaFilterButtonTapped(_ sender: UIButton) {
+        guard let cgImage = applyFilter(CIFilter(name: "CISepiaTone")) else { return }
+        imageView.image = UIImage(cgImage: cgImage)
+    }
+    
+    @IBAction func noirFilterButtonTapped(_ sender: UIButton) {
+        guard let cgImage = applyFilter(CIFilter(name: "CIPhotoEffectNoir")) else { return }
+        imageView.image = UIImage(cgImage: cgImage)
+    }
+    
+    @IBAction func monochromeFilterButtonTapped(_ sender: UIButton) {
+        guard let cgImage = applyFilter(CIFilter(name: "CIColorMonochrome"), andColor: CIColor.blue) else { return }
+        imageView.image = UIImage(cgImage: cgImage)
+    }
+    
+    @IBAction func invertFilterButtonTapped(_ sender: UIButton) {
+        guard let cgImage = applyFilter(CIFilter(name: "CIColorInvert")) else { return }
+        imageView.image = UIImage(cgImage: cgImage)
+    }
+    
+    @IBAction func vignetteFilterButtonTapped(_ sender: UIButton) {
+        guard let cgImage = applyFilter(CIFilter(name: "CIVignetteEffect")) else { return }
+        imageView.image = UIImage(cgImage: cgImage)
     }
 }
 
